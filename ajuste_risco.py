73229a82c89a31af7015bc8a3a0d236c56cf499d
olavo <olavosaires@gmail.com>
2025-04-15 18:49:08 -0300

    ajuste risco

--- ajuste_risco.py
+++ ajuste_risco.py
@@ -1,17 +1,23 @@
 import csv
 
-from validacao_tpcliente import dadcad_path
-
 risco_ref_path = 'risco.csv'
-dacad_path = 'dadcad.csv'
+dadcad_path = 'dadcad.csv'
 output_path = 'dadcad_risco_ajustado.csv'
+
+#COLUNAS - ARQUIVO REFERÊNCIA
+coluna_codigo_referencia = 'cliente_col'
+coluna_risco_referencia =  'risco_col'
+
+#COLUNAS - ARQUIVO DADCAD
+coluna_codigo_dadcad = 'cliente_col_dadcad'
+coluna_risco_dadcad = 'risco_col_dadcad'
 
 risk_mapping = {}
 with open(risco_ref_path, mode='r', newline='', encoding='utf-8') as risco_file:
     reader = csv.DictReader(risco_file, delimiter=';')
     for row in reader:
-        client_code = row['client_code']
-        risk_value = row['risk']
+        client_code = row[coluna_codigo_referencia]
+        risk_value = row[coluna_risco_referencia]
         risk_mapping[client_code] = risk_value
 
 
@@ -20,9 +26,9 @@ with open(dadcad_path, mode='r+', newline='', encoding='utf-8') as dadcad_file:
     reader = csv.DictReader(dadcad_file, delimiter=';')
     fieldnames = reader.fieldnames
     for row in reader:
-        client_code = row['client_code']
+        client_code = row[coluna_codigo_dadcad]
         if client_code in risk_mapping:
-            row['risk'] = risk_mapping[client_code]
+            row[coluna_risco_dadcad] = risk_mapping[client_code]
         updated_rows.append(row)
 
 
