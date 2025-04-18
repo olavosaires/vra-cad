5329e80d2f97ff579286f6776d7a337a8bd05dd1
olavo <olavosaires@gmail.com>
2025-04-18 12:33:41 -0300

    ajustes

--- validacao_vazios.py
+++ validacao_vazios.py
@@ -4,6 +4,11 @@ input_file_name = 'colunas.csv'
 output_file_name = 'output_validacao_vazios.csv'
 
 contador = 0;
+contador_blankspace = 0;
+
+blankspace_register = {}
+client_code_column = 'CodigoCliente'
+
 
 with open(input_file_name, mode='r', encoding='utf-8') as infile, \
      open(output_file_name, mode='w', encoding='utf-8', newline='') as outfile:
@@ -15,10 +20,24 @@ with open(input_file_name, mode='r', encoding='utf-8') as infile, \
 
     for row in reader:
         contador += 1
+        apontamento = False
         for col in reader.fieldnames:
-            if row.get(col) == '':
-                writer.writerow(row)
+            if row.get(col).isspace():
+                apontamento = True
+                contador_blankspace += 1
+                blankspace_register = {
+                    row.get(client_code_column) : col,
+                }
+            elif row.get(col) == '':
+                apontamento = True
                 break
+            else:
+                continue
+        if apontamento:
+            writer.writerow(row)
+
+
         print(f'🔄 guenta aí, to indo: {contador}', end='\r', flush=True)
 
 print('Pronto')
+print(f'Blanks: {contador_blankspace}')
